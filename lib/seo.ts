fb6d5ac9a351688f8005334eb972e452e7135ce2
vawnaire <63461297+vawnair@users.noreply.github.com>
2022-11-20 13:35:14 +0200

    Update seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -23,7 +23,7 @@ export function useSeoProps(
 			type: 'website',
 			images: [
 				{
-					url: 'https://enviroment.cloud/banner.png',
+					url: ' ',
 					alt: description,
 					width: 1280,
 					height: 720,
